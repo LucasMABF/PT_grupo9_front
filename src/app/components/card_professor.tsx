db2777f0f4bff69df1963cebb78a1cc987d26afc
Lucas Mendes <70616909+LucasMABF@users.noreply.github.com>
2024-12-13 17:57:29 -0300

    feat: tela de feed finalizada

--- src/app/components/card_professor.tsx
+++ src/app/components/card_professor.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Image from "next/image";
 import ProfilePic from "/public/media/profile_pic.svg";
 
-const CardProfessor = () => {
+interface Props{
+  nome: string;
+  materia: string;
+}
+
+const CardProfessor = (props: Props) => {
   // adicionar dados argumento
   return(
     <div className="bg-color1 p-3 w-40 text-center rounded-lg flex flex-col shrink-0 hover: cursor-pointer hover:scale-105 duration-100">
       <Image className="rounded-xl" src={ProfilePic} alt="logo UnB"/>
-      <span className="mt-1"> Nome </span>
-      <span> matéria </span>
+      <span className="mt-1"> {props.nome} </span>
+      <span> {props.materia} </span>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,30 @@
 import CardProfessor from "./components/card_professor";
+import DropDown from "./components/dropdown"
 import Image from "next/image";
 import SearchIcon from "/public/media/search_icon.svg";
 
 export default function Home() {
-  // adicionar simbolo pesquisa
   return (
-    <main className="grow flex flex-col gap-5 py-5 px-0.5">
+    <main className="grow flex flex-col gap-5 py-5 px-0.5 relative">
 
-      <div className="self-end flex bg-white rounded-lg p-1 gap-0 m-1.5 border-[3px] border-green-700 has-[:focus]:border-green-900">
+      <div className="absolute top-1.5 right-1.5 self-end flex w-1/3 max-w-96 bg-white rounded-lg p-1 gap-0 m-1.5 border-[3px] border-green-700 has-[:focus]:border-green-900">
         <Image src={SearchIcon} alt="ícone de pesquisa"/>
-        <input type="text" placeholder="Buscar Professor(a) ou matéria" className="w-96 p-1 text-gray-700 outline-0"/>     
+        <input type="text" placeholder="Buscar Professor(a) ou matéria" className="w-full  p-1 text-gray-700 outline-0"/>     
       </div>
       <div className="m-1">
-        <h2 className="text-3xl">Recentemente avaliados</h2> 
+        <h2 className="md:text-3xl text-2xl">Recentemente avaliados</h2> 
 
-        <section className="flex gap-5 m-2 overflow-x-auto p-1.5">
-          {Array.from({length: 15}).map((_, index) => <CardProfessor key={index}/>)}
+        <section className="flex gap-5 m-2 overflow-x-scroll p-1.5">
+          {Array.from({length: 15}).map((_, index) => <CardProfessor key={index} materia="matéria" nome="Nome"/>)}
         </section>
       </div>
-
       <div className="m-1">
-        <h2 className="text-3xl">Todos os professores</h2>
-        <section className="flex gap-5 m-2 overflow-x-auto p-1.5">
-          {Array.from({length: 15}).map((_, index) => <CardProfessor key={index}/>)}
+        <div className="flex justify-between">
+          <h2 className="md:text-3xl text-2xl">Todos os professores</h2>
+          <DropDown options={["Nome", "Matéria", "Departamento", "Recentes", "Antigos"]}/>
+        </div>
+        <section className="flex gap-5 m-2 flex-wrap  p-1.5 justify-center">
+          {Array.from({length: 21}).map((_, index) => <CardProfessor key={index} materia="matéria" nome="Nome"/>)}
         </section>
       </div>
     </main>
