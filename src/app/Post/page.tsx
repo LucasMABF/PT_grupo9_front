d93bac77a745c8d1e8c7a4c3003540ec41142675
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2024-12-15 16:26:42 -0300

    Acrescentando o botão de voltar

--- src/app/Post/page.tsx
+++ src/app/Post/page.tsx
@@ -1,45 +1,16 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-
-interface Post {
-  id: number;
-  body: string;
-  postId: number;
-  user: { id: number; username: string };
-}
 
 export default function Post() {
-  const [ShowPost, setShowPost] = useState(false);
-  const [Post, setPost] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch("https://dummyjson.com/comments");
-        const data = await response.json();
-        setPost(data.comments);
-      } catch (error) {
-        console.error("Erro ao buscar os itens:", error);
-      }
-    };
-    fetchPosts();
-  }, []);
-
   return (
     <>
-      {ShowPost ? (
-        <div>
-          {Post.map((post) => (
-            <div key={post.id}>
-              <p>{post.body}</p>
-              <span>By: {post.user.username}</span>
-            </div>
-          ))}
-        </div>
-      ) : null}
+      <button className="back-button absolute w-12 h-12 rounded-full bg-white flex justify-center items-center m-20 cursor-pointer shadow-lg hover:w-20 transition-all">
+        <div className="w-2.5 h-2.5 border-l-2 border-t-2 border-black rotate-[-45deg] hover:border-blue-600"></div>
+      </button>
 
       <main className="body bg-gray-100 justify-center items-center w-screen h-screen flex flex-col">
+        {/* botao de voltar */}
+
         {/* Comment Box */}
         <div className="bg-white border border-x-1 border-black h-full max-w-full mx-auto py-4 px-4">
           <div className="bg-blue-300 px-10 py-10 rounded-[50px] shadow-lg">
@@ -82,7 +53,7 @@ export default function Post() {
               Adorei esse professor, ele deixa fazer a prova em grupo e também
             </p>
             <div className="w-full flex justify-between items-center">
-              <button onClick={() => setShowPost(true)}>2 comentários</button>
+              <button>2 comentários</button>
               <div className="flex gap-4">
                 <button>Editar</button>
                 <button>Excluir</button>
