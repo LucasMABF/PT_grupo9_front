cab48d90cf2c0f944fc12d40e59d754b663fbcb6
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2024-12-15 18:48:51 -0300

    Colocando a avaliação dos outros usuários

--- src/app/Post/page.tsx
+++ src/app/Post/page.tsx
@@ -14,50 +14,103 @@ export default function Post() {
         {/* Comment Box */}
         <div className="bg-white border border-x-1 border-black h-full max-w-full mx-auto py-4 px-4">
           <div className="bg-blue-300 px-10 py-10 rounded-[50px] shadow-lg">
-            <div className="flex items-center">
-              <Image
-                width={40}
-                height={40}
-                src="/profile-picture.webp"
-                alt="Avatar"
-                className="post-avatar w-10 h-10 rounded-full mr-3"
-              />
-              <div>
-                <span className="user-name text-lg font-bold">
-                  Jacinto Pinto
-                </span>
-                <span className="estrelas text-lg ml-3">
-                  <span className="estrela1 m-0.5 text-yellow-500">
-                    &#9733;
+            <div className="border-b-2 border-black w-full">
+              <div className="flex items-center ">
+                <Image
+                  width={40}
+                  height={40}
+                  src="/profile-picture.webp"
+                  alt="Avatar"
+                  className="post-avatar w-10 h-10 rounded-full mr-3"
+                />
+                <div>
+                  <span className="user-name text-lg font-bold text-gray-900">
+                    Jacinto Pinto
                   </span>
-                  <span className="estrela2 m-0.5 text-yellow-500">
-                    &#9733;
+                  <span className="estrelas text-lg ml-3">
+                    <span className="estrela1 m-0.5 text-yellow-500">
+                      &#9733;
+                    </span>
+                    <span className="estrela2 m-0.5 text-yellow-500">
+                      &#9733;
+                    </span>
+                    <span className="estrela3 m-0.5 text-yellow-500">
+                      &#9733;
+                    </span>
+                    <span className="estrela4 m-0.5">&#9733;</span>
+                    <span className="estrela5 m-0.5">&#9733;</span>
                   </span>
-                  <span className="estrela3 m-0.5 text-yellow-500">
-                    &#9733;
-                  </span>
-                  <span className="estrela4 m-0.5">&#9733;</span>
-                  <span className="estrela5 m-0.5">&#9733;</span>
-                </span>
+                </div>
+              </div>
+
+              <span className="post-info text-sm text-gray-500">
+                <span className="data">23/12/2024</span>, às{" "}
+                <span className="hora">21:42</span> -{" "}
+                <span className="professor">Dumbledore</span> -{" "}
+                <span className="disciplina">Magia Negra</span>
+              </span>
+
+              <p className="post-text my-4 text-gray-800">
+                Adorei esse professor, ele deixa fazer a prova em grupo e também
+              </p>
+              <div className="w-full flex justify-between items-center">
+                <button>2 comentários</button>
+                <div className="flex gap-4">
+                  <button>Editar</button>
+                  <button>Excluir</button>
+                </div>
               </div>
             </div>
 
-            <span className="post-info text-sm text-gray-500">
-              <span className="data">23/12/2024</span>, às{" "}
-              <span className="hora">21:42</span> -{" "}
-              <span className="professor">Dumbledore</span> -{" "}
-              <span className="disciplina">Magia Negra</span>
-            </span>
-
-            <p className="post-text my-4 text-gray-800">
-              Adorei esse professor, ele deixa fazer a prova em grupo e também
-            </p>
-            <div className="w-full flex justify-between items-center">
-              <button>2 comentários</button>
-              <div className="flex gap-4">
-                <button>Editar</button>
-                <button>Excluir</button>
+            {/*Comentário dos outros usuário */}
+            <div>
+              <div className="flex my-4 items-center">
+                <Image
+                  width={40}
+                  height={40}
+                  src="/Perfil_secundario.jpg"
+                  alt="Avatar"
+                  className="post-avatar w-10 h-10 rounded-full mr-3"
+                />
+                <div>
+                  <span className="user-name font-bold text-gray-900">
+                    Baby Dinossauro
+                  </span>
+                  <span className="text-sm ml-3 text-gray-500">
+                    <span className="data ">25/01/2025</span>, ás{" "}
+                    <span className="hora ">00:00</span>
+                  </span>
+                </div>
               </div>
+
+              <p className="post-text my-2 pl-10 text-gray-800">
+                Valeu pela dica
+              </p>
+
+              {/*Comentário de outro usuário */}
+              <div className="border-t border-black my-4"></div>
+              <div className="flex my-4 items-center">
+                <Image
+                  width={40}
+                  height={40}
+                  src="/Perfil_secundario.jpg"
+                  alt="Avatar"
+                  className="post-avatar w-10 h-10 rounded-full mr-3"
+                />
+                <div>
+                  <span className="user-name font-bold text-gray-900">
+                    Baby Dinossauro
+                  </span>
+                  <span className="text-sm ml-3 text-gray-500">
+                    <span className="data ">25/01/2025</span>, ás{" "}
+                    <span className="hora ">00:00</span>
+                  </span>
+                </div>
+              </div>
+
+              <p className="post-text my-2 pl-10 text-gray-800">
+                Semestre que vem tentarei pegar matéria com esse professor
+              </p>
             </div>
           </div>
         </div>
