ad8e66e14ef37ad1b5c2869bce3c319ca8614b31
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2024-12-19 19:24:30 -0300

    criando modal de deletar comentário

--- src/components/Modal-Excluir-comentáio.tsx
+++ src/components/Modal-Excluir-comentáio.tsx
@@ -10,11 +10,19 @@ const ModalExcluirComentario: React.FC<ModalExcluirComentarioProps> = ({
 }) => {
   return (
     <>
+      icon-lixeira.png
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <form className="evaluation-container bg-green-700 flex flex-col items-center justify-center px-10 py-12 w-1/4 rounded-xl">
-          <h1 className="text-black text-2xl">
-            Excluir avaliação com comentário?
-          </h1>
+        <form className="evaluation-container bg-green-700 flex flex-col items-center justify-center px-10 py-8 w-1/4 rounded-xl">
+          <div className="my-4 flex flex-col items-center justify-center bg-blue-100 text-black p-4 rounded-lg w-full h-64">
+            <img
+              width={100}
+              height={100}
+              src="/icon-lixeira.png"
+              alt="Avatar"
+              className="icon  w-100 h-100"
+            ></img>
+            <h1 className="text-black my-5 text-2xl">Você tem certeza ?</h1>
+          </div>
           <div className="container-footer my-5 flex justify-end items-end">
             <div className=" flex">
               <div
